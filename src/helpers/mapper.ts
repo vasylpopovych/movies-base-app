79e7024422ced854abbc33bc4e66bdf7e225a1a2
Vasyl Popovych <popovich.w.ppc@gmail.com>
2023-02-16 00:08:48 +0200

    CREATE: create mapper

---
+++ src/helpers/mapper.ts
@@ -0,0 +1,16 @@
+import Movie from '../types/Movie';
+
+export const mapper = (dataFromApi: []): [] => {
+    const result: [] = [];
+    for (const movie of dataFromApi) {
+        const obj: Movie = {
+            title: movie['title'],
+            poster_path: movie['poster_path'],
+            backdrop_path: movie['backdrop_path'],
+            overview: movie['overview'],
+            release_date: movie['release_date'],
+        };
+        result.push(obj);
+    }
+    return result;
+};
