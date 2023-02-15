faf1486f0148c7412ed394f70c665c10ca1e288e
Vasyl Popovych <popovich.w.ppc@gmail.com>
2023-02-15 22:56:26 +0200

    DELETE: delete img size styles for movie card

--- src/components/movieCard.ts
+++ src/components/movieCard.ts
@@ -6,7 +6,7 @@ const createMovieCard = (
     const movieCard = document.createElement('div');
     movieCard.className = 'col-lg-3 col-md-4 col-12 p-2';
     movieCard.innerHTML = `<div class="card shadow-sm">
-    <img class="poster" src="https://image.tmdb.org/t/p/original/${imgUrl}" />
+    <img src="https://image.tmdb.org/t/p/original/${imgUrl}" />
     <svg xmlns="http://www.w3.org/2000/svg" stroke="red" fill="#ff000078" width="50" height="50"
                                 class="bi bi-heart-fill position-absolute p-2" viewBox="0 -2 18 22">
                                 <path fill-rule="evenodd"
